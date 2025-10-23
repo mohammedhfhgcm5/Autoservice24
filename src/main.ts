c453d32ff81182c7ce966952d4ef51dd7ad4bf94
mohammedhfhgcm5 <hfhgcm5@gmail.com>
2025-10-23 17:15:02 +0300

    finishh

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+ app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
 //2BrnIvhBYEPPYWWk
 
-//mongodb+srv://hfhgcm5_db_user:<db_password>@autoservice24.bjn5m4q.mongodb.net/?retryWrites=true&w=majority&appName=autoservice24
